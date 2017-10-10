a5b71b72e14d86d58385c313d706063a3743d2f1
mattyDunn <31864601+mattyDunn@users.noreply.github.com>
2017-10-10 13:35:20 -0600

    UI Changes

--- IOSHEXtoRGB/IOSHEXtoRGB/ViewController.designer.cs
+++ IOSHEXtoRGB/IOSHEXtoRGB/ViewController.designer.cs
@@ -1,17 +1,64 @@
-﻿//
-// This file has been generated automatically by MonoDevelop to store outlets and
-// actions made in the Xcode designer. If it is removed, they will be lost.
-// Manual changes to this file may not be handled correctly.
+// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
 //
 using Foundation;
+using System;
+using System.CodeDom.Compiler;
 
 namespace IOSHEXtoRGB
 {
-    [Register("ViewController")]
+    [Register ("ViewController")]
     partial class ViewController
     {
-        void ReleaseDesignerOutlets()
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel blueLabel { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton convertButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel greenLabel { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField hexTextFeild { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel redLabel { get; set; }
+
+        void ReleaseDesignerOutlets ()
         {
+            if (blueLabel != null) {
+                blueLabel.Dispose ();
+                blueLabel = null;
+            }
+
+            if (convertButton != null) {
+                convertButton.Dispose ();
+                convertButton = null;
+            }
+
+            if (greenLabel != null) {
+                greenLabel.Dispose ();
+                greenLabel = null;
+            }
+
+            if (hexTextFeild != null) {
+                hexTextFeild.Dispose ();
+                hexTextFeild = null;
+            }
+
+            if (redLabel != null) {
+                redLabel.Dispose ();
+                redLabel = null;
+            }
         }
     }
 }
